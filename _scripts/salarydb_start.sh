d9764faaa0859bee07978cd53db77b7f5da3cec2
Peter Siemens <peterjsiemens@gmail.com>
2016-08-15 17:00:35 -0700

    Use BASH_SOURCE to call gunicorn_start.sh

--- _scripts/salarydb_start.sh
+++ _scripts/salarydb_start.sh
@@ -3,4 +3,4 @@
 # Start the server
 sudo chown salarydb ${BASH_SOURCE%/*}/gunicorn_start.sh
 sudo chmod u+x ${BASH_SOURCE%/*}/gunicorn_start.sh
-sudo -u salarydb -H sh -c "_scripts/gunicorn_start.sh"
+sudo -u salarydb -H sh -c "${BASH_SOURCE%/*}/gunicorn_start.sh"
