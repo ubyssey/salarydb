b50fc580b7709595f1d613713ae869b9ac6f42b4
Peter Siemens <peterjsiemens@gmail.com>
2016-08-15 17:37:27 -0700

    Add success message to gunicorn_start script

--- _scripts/gunicorn_start.sh
+++ _scripts/gunicorn_start.sh
@@ -17,16 +17,14 @@ source bin/activate
 export PYTHONPATH=$DJANGO_PROJECT_DIR:$PYTHONPATH
 
 # Install dependencies
-
 pip install -r /var/webapps/salarydb/requirements.txt
-
-# Start gunicorn daemon
-
-echo "Starting $NAME as `whoami`"
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
 test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start gunicorn daemon
+echo "Starting $NAME as `whoami`"
 
 exec bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --name $NAME \
@@ -36,3 +34,5 @@ exec bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
   --log-file $LOGFILE \
   --daemon \
   --pid $PIDFILE
+
+echo "Successfully started $NAME"
