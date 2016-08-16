399954f7999a80aa9bcba055725acc100d1fe8d0
Peter Siemens <peterjsiemens@gmail.com>
2016-08-16 12:25:00 -0700

    Add new start script

--- _scripts/salarydb_start.sh
+++ _scripts/salarydb_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+GUNICORN_SCRIPT=/var/webapps/salarydb/_scripts/gunicorn_start.sh
+
 # Start the server
-sudo chown salarydb ${BASH_SOURCE%/*}/gunicorn_start.sh
-sudo chmod u+x ${BASH_SOURCE%/*}/gunicorn_start.sh
-sudo -u salarydb -H sh -c "${BASH_SOURCE%/*}/gunicorn_start.sh"
+sudo chown salarydb $GUNICORN_SCRIPT
+sudo chmod u+x $GUNICORN_SCRIPT
+
+sudo supervisorctl start salarydb
