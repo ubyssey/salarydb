2b074d1db100dd778d5535c62337855a84750f0e
Peter Siemens <peterjsiemens@gmail.com>
2017-11-02 23:34:21 -0700

    Clean up models

--- salarydb/models.py
+++ salarydb/models.py
@@ -1,17 +1,25 @@
-from django.db.models import Model, CharField, ForeignKey, IntegerField
+from django.db.models import Model, CharField, ForeignKey, SlugField, IntegerField
+
+CAMPUS_CHOICES = [
+    ('van', 'Vancouver'),
+    ('oka', 'Okanagan'),
+]
+
+GENDER_CHOICES = [
+    ('m', 'Male'),
+    ('f', 'Female'),
+]
 
 class Employee(Model):
     first_name = CharField(max_length=255)
     last_name = CharField(max_length=255)
-    position = ForeignKey('Position', blank=True, null=True)
-    faculty = ForeignKey('Faculty', blank=True, null=True)
-    department = ForeignKey('Department', blank=True, null=True)
-    campus = CharField(max_length=255, choices=[('van', 'Vancouver'), ('oka', 'Okanagan')], blank=True, null=True)
-    gender = CharField(max_length=1, choices=[('m', 'Male'), ('f', 'Female')], blank=True, null=True) # 'm' or 'f'
-    remuneration = IntegerField()
-    expenses = IntegerField()
+    position = ForeignKey('Position', null=True)
+    faculty = ForeignKey('Faculty', null=True)
+    department = ForeignKey('Department', null=True)
+    campus = CharField(max_length=255, choices=CAMPUS_CHOICES, null=True)
+    gender = CharField(max_length=1, choices=GENDER_CHOICES, null=True)
 
-    slug = CharField(max_length=255)
+    slug = SlugField(max_length=255)
 
     rating = IntegerField(default=0)
     num_ratings = IntegerField(default=0)
@@ -20,7 +28,7 @@ class Employee(Model):
         ordering = ['last_name']
 
     def __str__(self):
-        return self.last_name + ', ' + self.first_name
+        return self.full_name()
 
     def full_name(self):
         return self.first_name + ' ' + self.last_name
@@ -29,11 +37,11 @@ class Employee(Model):
         if self.num_ratings > 0:
             return float(self.rating) / self.num_ratings
         else:
-            return False
+            return None
 
     def full_department(self):
         if self.department and self.faculty:
-            return "%s - %s" % (self.faculty.full_name, self.department.name)
+            return '%s - %s' % (self.faculty.full_name, self.department.name)
         elif self.department:
             return self.department.name
         else:
@@ -41,6 +49,7 @@ class Employee(Model):
 
     def url(self):
         first_name = self.first_name.split()
+
         if len(first_name) > 0:
             first_name = first_name[0]
         else:
@@ -49,6 +58,15 @@ class Employee(Model):
             'first_name': first_name.lower(),
             'last_name': self.last_name.lower()
         }
+
+class Salary(Model):
+    employee = ForeignKey('Employee')
+    year = IntegerField()
+    remuneration = IntegerField()
+    expenses = IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'salaries'
 
 class Position(Model):
     name = CharField(max_length=255, blank=True, null=True)
@@ -63,19 +81,19 @@ class Position(Model):
 class Faculty(Model):
     full_name = CharField(max_length=255)
     short_name = CharField(max_length=255)
-    campus = CharField(max_length=255, choices=[('van', 'Vancouver'), ('oka', 'Okanagan')], blank=True, null=True)
+    campus = CharField(max_length=255, choices=CAMPUS_CHOICES, null=True)
 
     def __str__(self):
         return self.full_name
 
     class Meta:
-        verbose_name_plural = "faculties"
+        verbose_name_plural = 'faculties'
 
 class Department(Model):
     name = CharField(max_length=255, blank=True, null=True)
-    faculty = ForeignKey('Faculty', blank=True, null=True)
-    parent = ForeignKey('Department', blank=True, null=True)
-    campus = CharField(max_length=255, choices=[('van', 'Vancouver'), ('oka', 'Okanagan')], blank=True, null=True)
+    faculty = ForeignKey('Faculty', null=True)
+    parent = ForeignKey('Department', null=True)
+    campus = CharField(max_length=255, choices=CAMPUS_CHOICES, null=True)
     raw_name = CharField(max_length=255)
 
     class Meta:
